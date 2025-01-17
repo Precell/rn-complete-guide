167bfa7d52fce604dffda7041f7403a17b9ce9e3
Precell <precell.3saini@gmail.com>
2025-01-17 09:56:32 +0200

    rn complete guide basics guide

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([])
+  
+  const goalInputHandler = (enteredText)=>{
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = ()=>{
+    console.log(enteredGoal);
+    // setCourseGoals([...courseGoals, enteredGoal])    
+    setCourseGoals(currentGoals =>[...currentGoals, enteredGoal])   
+    setEnteredGoal('') 
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 30 }}>
+      <View style={styles.screen }>
+        <TextInput placeholder='Course Goal' value={enteredGoal} onChangeText={goalInputHandler} style={styles.inputContainer} />
+        <Button title='ADD' onPress={addGoalHandler}/>
+      </View>
+      <ScrollView >
+        {
+          courseGoals.map((goal, i) => <View style={styles.listItem} key={i}><Text  >{goal}</Text></View>)
+        }
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: { flexDirection: 'row', width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
+  inputContainer:{ borderBottomColor: 'red',
+    borderWidth: 1,
+    padding: 10,
+    width: 200
+  },
+  listItem:{
+    padding:10,
+    backgroundColor:'#ccc',
+    borderColor:'black',
+    marginVertical:10
+  }
+
 });
+
